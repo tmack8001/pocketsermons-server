55d63dad4d531ad8147ac6594cb42cfbd268658b
Trevor Mack <tmack@groupon.com>
2015-04-14 09:20:11 -0700

    visualization improvements new sermon creation workflow

--- public/javascripts/pocketsermons/controllers/sermonController.js
+++ public/javascripts/pocketsermons/controllers/sermonController.js
@@ -1,27 +1,56 @@
 angular.module('pocketsermons')
-    .controller('SermonController', ['$scope', 'Sermons', function ($scope, Sermons) {
+    .controller('SermonController', ['$scope', 'Sermons', '$location', function ($scope, Sermons, $location) {
         $scope.sermons = Sermons.query();
 
         // TODO: need to figure out how to create a new Sermon fully since API requires various conditions to be met
         $scope.save = function () {
             if (!$scope.newSermon || $scope.newSermon.length < 1) return;
-            var object = new Sermons({ title: $scope.newSermon, completed: false });
-
-            object.$save(function () {
-                $scope.sermons.push(object.sermon);
-                $scope.newSermon = ''; // clear textbox
-            });
+            $location.url('/sermons/new?title=' + $scope.newSermon + '&permalink=' + $scope.newSermon.replace(' ', '-').toLowerCase());
         };
     }])
 
     .controller('SermonDetailCtrl', ['$scope', '$routeParams', 'Sermons', '$location', function ($scope, $routeParams, Sermons, $location) {
-        Sermons.get({id: $routeParams.id}, function(response) {
-            $scope.sermon = response.sermon;
-        });
-        $scope.editing = false;
+        if ($routeParams.id === 'new') {
+            var sermon = { title: $routeParams.title, permalink: $routeParams.permalink};
+            $scope.sermon = sermon;
+            $scope.new = true;
+            $scope.editing = true;
+        } else {
+            Sermons.get({id: $routeParams.id}, function(response) {
+                $scope.sermon = response.sermon;
+            });
+            $scope.editing = false;
+            $scope.new = false;
+        }
+
+        $scope.save = function () {
+            console.log('saving new sermon');
+            console.log($scope.sermon);
+            // verify required info
+            if (!$scope.sermon || !$scope.sermon.permalink ||
+            !$scope.sermon.title || !$scope.sermon.date || !$scope.sermon.videoUri) return;
+
+            var object = new Sermons({
+                permalink: $scope.sermon.permalink,
+                title: $scope.sermon.title,
+                date: $scope.sermon.date,
+                description: $scope.sermon.description,
+                videoUri: $scope.sermon.videoUri,
+                series: $scope.sermon.series._id,
+                church: $scope.sermon.church._id,
+                completed: false
+            });
+            console.log(object);
+            object.$save(function () {
+                $scope.newSermon = ''; // clear textbox
+                $location.url('/sermons');
+            });
+        };
 
         $scope.update = function(){
             var sermon = $scope.sermon;
+
+            console.log(sermon);
 
             sermon.series = sermon.series._id;
             sermon.church = sermon.church._id;
